c8b00f70d4689831f7cd9a7c8d6b6622129b2521
Philip Helger <philip@helger.com>
2015-09-10 17:18:06 +0200

    API beautifications

--- peppol-yellow-pages-api/src/main/java/com/helger/pyp/storage/PYPStorageManager.java
+++ peppol-yellow-pages-api/src/main/java/com/helger/pyp/storage/PYPStorageManager.java
@@ -34,6 +34,7 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.search.SimpleCollector;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
@@ -101,6 +102,13 @@ public final class PYPStorageManager implements Closeable
     return new Term (CPYPStorage.FIELD_PARTICIPANTID, aParticipantID.getURIEncoded ());
   }
 
+  private static Query _andNotDeleted (@Nonnull final Query aQuery)
+  {
+    return new BooleanQuery.Builder ().add (aQuery, Occur.MUST)
+                                      .add (new TermQuery (new Term (CPYPStorage.FIELD_DELETED)), Occur.MUST_NOT)
+                                      .build ();
+  }
+
   public boolean containsEntry (@Nullable final IPeppolParticipantIdentifier aParticipantID) throws IOException
   {
     if (aParticipantID == null)
@@ -111,12 +119,8 @@ public final class PYPStorageManager implements Closeable
       if (aSearcher != null)
       {
         // Search only documents that do not have the deleted field
-        final TopDocs aTopDocs = aSearcher.search (new BooleanQuery.Builder ().add (new TermQuery (new Term (CPYPStorage.FIELD_PARTICIPANTID,
-                                                                                                             aParticipantID.getURIEncoded ())),
-                                                                                    Occur.MUST)
-                                                                              .add (new TermQuery (new Term (CPYPStorage.FIELD_DELETED)),
-                                                                                    Occur.MUST_NOT)
-                                                                              .build (),
+        final TopDocs aTopDocs = aSearcher.search (_andNotDeleted (new TermQuery (new Term (CPYPStorage.FIELD_PARTICIPANTID,
+                                                                                            aParticipantID.getURIEncoded ()))),
                                                    1);
         if (aTopDocs.totalHits > 0)
           return Boolean.TRUE;
@@ -202,7 +206,7 @@ public final class PYPStorageManager implements Closeable
   }
 
   @Nonnull
-  public final List <StoredDocument> getAllDocuments (@Nonnull final Term aTerm) throws IOException
+  public final List <StoredDocument> getAllDocuments (@Nonnull final Query aQuery) throws IOException
   {
     return m_aLucene.runAtomic ( () -> {
       final IndexSearcher aSearcher = m_aLucene.getSearcher ();
@@ -210,7 +214,7 @@ public final class PYPStorageManager implements Closeable
       {
         // Search only documents that do not have the deleted field
         final List <Document> aTargetList = new ArrayList <> ();
-        aSearcher.search (new TermQuery (aTerm), new AllDocumentsCollector (aTargetList));
+        aSearcher.search (aQuery, new AllDocumentsCollector (aTargetList));
         return aTargetList.stream ().map (d -> StoredDocument.create (d)).collect (Collectors.toList ());
       }
       return new ArrayList <> ();
@@ -218,15 +222,21 @@ public final class PYPStorageManager implements Closeable
   }
 
   @Nonnull
+  public final List <StoredDocument> getAllDeletedDocuments () throws IOException
+  {
+    return getAllDocuments (new TermQuery (new Term (CPYPStorage.FIELD_DELETED)));
+  }
+
+  @Nonnull
   public final List <StoredDocument> getAllDocumentsOfParticipant (@Nonnull final IPeppolParticipantIdentifier aParticipantID) throws IOException
   {
-    return getAllDocuments (new Term (CPYPStorage.FIELD_PARTICIPANTID, aParticipantID.getURIEncoded ()));
+    return getAllDocuments (new TermQuery (_createTerm (aParticipantID)));
   }
 
   @Nonnull
   public final List <StoredDocument> getAllDocumentsOfCountryCode (@Nonnull final String sCountryCode) throws IOException
   {
     ValueEnforcer.notNull (sCountryCode, "CountryCode");
-    return getAllDocuments (new Term (CPYPStorage.FIELD_COUNTRY_CODE, sCountryCode));
+    return getAllDocuments (new TermQuery (new Term (CPYPStorage.FIELD_COUNTRY_CODE, sCountryCode)));
   }
 }
