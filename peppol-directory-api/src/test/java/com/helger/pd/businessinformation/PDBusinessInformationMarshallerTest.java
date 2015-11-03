551bb02b3ee454a9de3b7d2728af5aa826fe83ba
phax <philip@helger.com>
2015-11-03 16:27:45 +0100

    Added standalone test file for testing simple reading

--- peppol-directory-api/src/test/java/com/helger/pd/businessinformation/PDBusinessInformationMarshallerTest.java
+++ peppol-directory-api/src/test/java/com/helger/pd/businessinformation/PDBusinessInformationMarshallerTest.java
@@ -1,10 +1,14 @@
 package com.helger.pd.businessinformation;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
+
+import com.helger.commons.io.resource.ClassPathResource;
 
 /**
  * Test class for class {@link PDBusinessInformationMarshaller}.
- * 
+ *
  * @author Philip Helger
  */
 public final class PDBusinessInformationMarshallerTest
@@ -12,6 +16,7 @@ public final class PDBusinessInformationMarshallerTest
   @Test
   public void testBasic ()
   {
-    new PDBusinessInformationMarshaller ();
+    final PDBusinessInformationMarshaller aMarshaller = new PDBusinessInformationMarshaller ();
+    assertNotNull (aMarshaller.read (new ClassPathResource ("business-information-test1.xml")));
   }
 }
