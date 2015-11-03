f333101f33d872574081a35b1e3dc0d2b2045de8
phax <philip@helger.com>
2015-11-03 23:01:33 +0100

    Version update

--- peppol-directory-indexer/src/main/java/com/helger/pd/indexer/mgr/SMPBusinessInformationProvider.java
+++ peppol-directory-indexer/src/main/java/com/helger/pd/indexer/mgr/SMPBusinessInformationProvider.java
@@ -21,31 +21,15 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.xml.XMLConstants;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Attr;
-import org.w3c.dom.DocumentType;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.ProcessingInstruction;
 
-import com.helger.commons.ValueEnforcer;
 import com.helger.commons.charset.CCharset;
 import com.helger.commons.microdom.IMicroElement;
 import com.helger.commons.microdom.IMicroNode;
-import com.helger.commons.microdom.MicroCDATA;
-import com.helger.commons.microdom.MicroComment;
-import com.helger.commons.microdom.MicroDocument;
-import com.helger.commons.microdom.MicroDocumentType;
-import com.helger.commons.microdom.MicroElement;
-import com.helger.commons.microdom.MicroEntityReference;
-import com.helger.commons.microdom.MicroProcessingInstruction;
-import com.helger.commons.microdom.MicroText;
 import com.helger.commons.microdom.serialize.MicroWriter;
+import com.helger.commons.microdom.util.MicroHelper;
 import com.helger.commons.url.SimpleURL;
 import com.helger.commons.url.URLHelper;
 import com.helger.commons.xml.XMLDebug;
@@ -76,105 +60,12 @@ public final class SMPBusinessInformationProvider implements IPDBusinessInformat
   private static final String URL_PART_SERVICES = "/services/";
   private static final Logger s_aLogger = LoggerFactory.getLogger (SMPBusinessInformationProvider.class);
 
-  // FIXME replace with MicroHelper version in ph-commons >= 6.2.1
-  @Nonnull
-  public static IMicroNode convertToMicroNode (@Nonnull final Node aNode)
-  {
-    ValueEnforcer.notNull (aNode, "Node");
-
-    IMicroNode ret;
-    final short nNodeType = aNode.getNodeType ();
-    switch (nNodeType)
-    {
-      case Node.DOCUMENT_NODE:
-      {
-        ret = new MicroDocument ();
-        break;
-      }
-      case Node.DOCUMENT_TYPE_NODE:
-      {
-        final DocumentType aDT = (DocumentType) aNode;
-        // inline DTDs are not supported yet
-        // aDT.getEntities ();
-        ret = new MicroDocumentType (aDT.getName (), aDT.getPublicId (), aDT.getSystemId ());
-        break;
-      }
-      case Node.ELEMENT_NODE:
-      {
-        final Element aElement = (Element) aNode;
-        final String sNamespaceURI = aElement.getNamespaceURI ();
-        final IMicroElement eElement = sNamespaceURI != null ? new MicroElement (sNamespaceURI, aElement.getLocalName ())
-                                                             : new MicroElement (aElement.getTagName ());
-        final NamedNodeMap aAttrs = aNode.getAttributes ();
-        if (aAttrs != null)
-        {
-          final int nAttrCount = aAttrs.getLength ();
-          for (int i = 0; i < nAttrCount; ++i)
-          {
-            final Attr aAttr = (Attr) aAttrs.item (i);
-            final String sAttrNamespaceURI = aAttr.getNamespaceURI ();
-            if (sAttrNamespaceURI != null)
-            {
-              // Ignore all "xmlns" attributes (special namespace URI!)
-              if (!XMLConstants.XMLNS_ATTRIBUTE_NS_URI.equals (sAttrNamespaceURI))
-                eElement.setAttribute (sAttrNamespaceURI, aAttr.getLocalName (), aAttr.getValue ());
-            }
-            else
-              eElement.setAttribute (aAttr.getName (), aAttr.getValue ());
-          }
-        }
-        ret = eElement;
-        break;
-      }
-      case Node.CDATA_SECTION_NODE:
-        ret = new MicroCDATA (aNode.getNodeValue ());
-        break;
-      case Node.TEXT_NODE:
-        ret = new MicroText (aNode.getNodeValue ());
-        break;
-      case Node.COMMENT_NODE:
-        ret = new MicroComment (aNode.getNodeValue ());
-        break;
-      case Node.ENTITY_REFERENCE_NODE:
-        ret = new MicroEntityReference (aNode.getNodeValue ());
-        break;
-      case Node.PROCESSING_INSTRUCTION_NODE:
-        final ProcessingInstruction aPI = (ProcessingInstruction) aNode;
-        ret = new MicroProcessingInstruction (aPI.getTarget (), aPI.getData ());
-        break;
-      case Node.ATTRIBUTE_NODE:
-        throw new IllegalArgumentException ("Unknown/unsupported node type: ATTRIBUTE_NODE");
-      case Node.ENTITY_NODE:
-        throw new IllegalArgumentException ("Unknown/unsupported node type: ENTITY_NODE");
-      case Node.DOCUMENT_FRAGMENT_NODE:
-        throw new IllegalArgumentException ("Unknown/unsupported node type: DOCUMENT_FRAGMENT_NODE");
-      case Node.NOTATION_NODE:
-        throw new IllegalArgumentException ("Unknown/unsupported node type: NOTATION_NODE");
-      default:
-        throw new IllegalArgumentException ("Unknown/unsupported node type: " + nNodeType);
-    }
-
-    // handle children recursively (works for different node types)
-    final NodeList aChildren = aNode.getChildNodes ();
-    if (aChildren != null)
-    {
-      final int nChildCount = aChildren.getLength ();
-      for (int i = 0; i < nChildCount; ++i)
-      {
-        final Node aChildNode = aChildren.item (i);
-        ret.appendChild (convertToMicroNode (aChildNode));
-      }
-    }
-
-    return ret;
-  }
-
   @Nullable
   public static PDBusinessInformationType extractBusinessInformation (@Nullable final ExtensionType aExtension)
   {
     if (aExtension != null && aExtension.getAny () != null)
     {
-      final IMicroNode aExtensionContainer = convertToMicroNode (aExtension.getAny ());
+      final IMicroNode aExtensionContainer = MicroHelper.convertToMicroNode (aExtension.getAny ());
       if (aExtensionContainer instanceof IMicroElement)
       {
         final IMicroElement eExtensionContainer = (IMicroElement) aExtensionContainer;
