e4d1ba5e00c0451cbb0a8b45312d393e49f53be3
daffyDukk <daniel.forsberg@dsv.su.se>
2021-03-08 09:58:12 +0100

    Dockerfile and docker-build.sh for the phoss-directory-publisher webapp

---
+++ docker-build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# This work is protected under copyrights held by the members of the
+# TOOP Project Consortium as indicated at
+# http://wiki.ds.unipi.gr/display/TOOP/Contributors
+# (c) 2018-2021. All rights reserved.
+#
+# This work is dual licensed under Apache License, Version 2.0
+# and the EUPL 1.2.
+#
+#  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+#
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved
+# by the European Commission - subsequent versions of the EUPL
+# (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+#         https://joinup.ec.europa.eu/software/page/eupl
+#
+
+
+
+version=0.9.4
+docker build --build-arg VERSION=$version -t phelger/phoss-directory-publisher:$version .
+docker tag phelger/phoss-directory-publisher:$version phelger/phoss-directory-publisher:latest
+#docker push phelger/phoss-directory-publisher:latest
+
