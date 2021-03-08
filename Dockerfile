715d9bedb75a5f4273346037c26ab8a51f06d4e8
Philip Helger <philip@helger.com>
2021-03-08 17:13:06 +0100

    Updated license header

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,20 @@
+#
+# Copyright (C) 2015-2021 Philip Helger (www.helger.com)
+# philip[at]helger[dot]com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 FROM tomcat:9-jdk11
 
 ARG VERSION="0.9.4"
@@ -5,7 +22,7 @@ ARG WAR_NAME=phoss-directory-publisher-${VERSION}.war
 
 WORKDIR $CATALINA_HOME/webapps
 
-ENV CATALINS_OPTS="$CATALINA_OPTS -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Djava.security.egd=file:/dev/urandom"
+ENV CATALINS_OPTS="$CATALINA_OPTS -Djava.security.egd=file:/dev/urandom"
 
 RUN wget -O ${WAR_NAME} https://github.com/phax/phoss-directory/releases/download/phoss-directory-parent-pom-${VERSION}/${WAR_NAME}
 
--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 #
-# This work is protected under copyrights held by the members of the
-# TOOP Project Consortium as indicated at
-# http://wiki.ds.unipi.gr/display/TOOP/Contributors
-# (c) 2018-2021. All rights reserved.
-#
-# This work is dual licensed under Apache License, Version 2.0
-# and the EUPL 1.2.
-#
-#  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# Copyright (C) 2015-2021 Philip Helger (www.helger.com)
+# philip[at]helger[dot]com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,16 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-#
-# Licensed under the EUPL, Version 1.2 or – as soon they will be approved
-# by the European Commission - subsequent versions of the EUPL
-# (the "Licence");
-# You may not use this work except in compliance with the Licence.
-# You may obtain a copy of the Licence at:
-#
-#         https://joinup.ec.europa.eu/software/page/eupl
 #
 
 
